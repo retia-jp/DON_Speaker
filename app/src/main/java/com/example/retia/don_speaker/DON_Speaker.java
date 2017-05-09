4fbfb48215689e18bd377048a5d26f0068b8facf
retia-jp <rethia.jp@gmail.com>
2017-05-09 14:05:28 +0900

    Initial commit

---
+++ app/src/main/java/com/example/retia/don_speaker/DON_Speaker.java
@@ -0,0 +1,13 @@
+package com.example.retia.don_speaker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DON_Speaker extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_don__speaker);
+    }
+}
